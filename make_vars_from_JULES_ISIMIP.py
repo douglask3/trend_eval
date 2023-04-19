da66502c39e9d22a94a923d21d584025a0186a4d
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2023-04-19 17:23:42 +0100

    tidying up and adding comments

--- make_vars_from_JULES_ISIMIP.py
+++ make_vars_from_JULES_ISIMIP.py
@@ -8,6 +8,54 @@
 # https://scitools.org.uk/iris/docs/v2.2/
 # https://scitools-iris.readthedocs.io/en/stable/index.html
 
+##################################
+## Set what you want to extract ##
+##################################
+
+## Which variables you want to import. Each element is a list. More than one item in teh list, and they get added together.
+varnames_in = [['pft-bdldcd', 'pft-bdlevgtrop', 'pft-bdlevgtemp', 'pft-ndldcd', 'pft-ndlevg'],
+               ['pft-c3grass', 'pft-c4grass'], 
+               ['burntarea-total']]
+
+## What your going to call the saved output
+varnames_out = ['tree_cover', 'grass_cover', 'burnt_area']
+
+## The start and end of the range of years you want
+year_range = [2000, 2003]
+
+## Where are you getting it from. List and change the directory depending on what you want.
+## for isimip stuff, varname is saved in teh file. So this is the bit of file before and after the variable name.
+Jules_ISIMIP_dir = "/scratch/hadea/isimip3a/u-cc669_isimip3a_fire/20CRv3-ERA5_obsclim/"
+Jules_ISIMIP_fileID = ["jules-vn6p3_20crv3-era5_obsclim_histsoc_default_", "_global_monthly_1901_2021.nc"]
+
+outDir = 'outputs/jules_isimip_processed/'
+outFile = 'fire_on_JULES_ISIMIP-'
+
+## Which country or continent. If you define a country, it one look at the continent. Also clips the data to the extent of that country or continent. Use None is you don't want any
+Country = 'Brazil'
+Continent = 'South America'
+#Continent options: 'South America', 'Oceania', 'Europe', 'Afria', 'North America', 'Asia' 
+
+## Which ecoregion from Olson.  Use None is you don't want any
+ecoregions = [3, 7, 8]
+
+#Ecoregion options. You can pick more than one
+#1 Tropical and subtropical moist broadleaf forests
+#2 Tropical and subtropical dry broadleaf forests
+#3 Tropical and suptropical coniferous forests
+#4 Temperate broadleaf and mixed forests
+#5 Temperate Coniferous Forest
+#6 Boreal forests / Taiga
+#7 Tropical and subtropical grasslands, savannas and shrublands
+#8 Temperate grasslands, savannas and shrublands
+#9 Flooded grasslands and savannas
+#10 Montane grasslands and shrublands
+#11 Tundra
+#12 Mediterranean Forests, woodlands and scrubs
+#13 Deserts and xeric shrublands
+#14 Mangroves
+
+
 
 #some useful imports:
 import os
@@ -44,7 +92,7 @@ def sort_time(cube, field, filename):
     cube.add_dim_coord(tcoord, 0) # might need to find this dimension
     iris.coord_categorisation.add_year(cube, 'time')
     iris.coord_categorisation.add_month(cube, 'time')
-    #cube = cube.extract(iris.Constraint(year=lambda cell: 2090 > cell))
+
     return(cube)
 
 def add_bounds(cube):
@@ -55,30 +103,18 @@ def add_bounds(cube):
     return(cube)
 
 
-Continent = 'South America'
-Country = 'Brazil'
-ecoregions = [3, 7, 8]
-
 def constrain_olson(cube):
-    #yay = shpreader.natural_earth(resolution='110m', 
-    #                                      category='cultural', name='admin_0_countries')
-    #shpfilename = shpreader.Reader('/data/users/arargles/model_inputs/eco_res_um/olson/tnc_terr_ecoregions.shp')
-    #ecoregions = shpfilename.records()
-    #ecoregion = next(ecoregions)
     biomes = iris.load_cube('data/wwf_terr_ecos_0p5.nc')
     mask = biomes.copy()
     mask.data[:] = 0.0
     for ecoreg in ecoregions: mask.data += biomes.data == ecoreg
     mask.data[mask.data.mask] = 0.0
     mask = mask.data == 0
-    #cube.data[mask.data == 0] = np.nan
+
     for layer in cube.data:
         layer.mask[mask] = False
         layer[mask] = np.nan
     return cube
-    #reducedbiome = biomes.extract(iris.Constraint(latitude=lambda cell: cell == np.array(ecoregions)))
-
-    #olson_file = shape.load_shp(shpfilename)
 
 def constrain_natural_earth(cube):
     shpfilename = shpreader.natural_earth(resolution='110m', 
@@ -107,24 +143,12 @@ def constrain_region(cube):
 
 
 
-varnames_in = [['pft-bdldcd', 'pft-bdlevgtrop', 'pft-bdlevgtemp', 'pft-ndldcd', 'pft-ndlevg'],
-            ['pft-c3grass', 'pft-c4grass'], ['burntarea-total']]
-varnames_out = ['tree_cover', 'grass_cover', 'burnt_area']
-
-year_range = [2000, 2003]
-
-
-#If loading in raw ISIMIP data:
-pwdproc= ["/scratch/hadea/isimip3a/u-cc669_isimip3a_fire/20CRv3-ERA5_obsclim/jules-vn6p3_20crv3-era5_obsclim_histsoc_default_", "_global_monthly_1901_2021.nc"]
-
-outDir = 'outputs/jules_isimip_processed/'
-outFile = 'fire_on_JULES_ISIMIP-'
-
 try: os.mkdir(outDir)
 except: pass
 
 def load_variable(variable, i = 0, multi = False):
-    cube_in = iris.load(pwdproc[0] + variable + pwdproc[1], callback=sort_time)[0]
+    file_in = Jules_ISIMIP_dir + Jules_ISIMIP_fileID[0] + variable + Jules_ISIMIP_fileID[1]
+    cube_in = iris.load(file_in, callback=sort_time)[0]
     cube = cube_in.copy()
     cube = cube.extract(iris.Constraint(year=lambda cell: year_range[0] < cell <= year_range[1]))
     cube = add_bounds(cube)
@@ -150,157 +174,15 @@ def load_variables(varname_in, varname_out = None):
     cube.var_name = varname_out
 
     out_file = outDir + '/' + outFile + '_' + varname_out + '.nc'
+    print(out_file)
     iris.save(cube, out_file)
     return cube
 
 all_variables = [load_variables(var_in, var_out) for var_in, var_out in zip(varnames_in, varnames_out)]
-browser()
 
-
-burnt_area_total=iris.load(pwdproc+"jules-vn6p3_20crv3-era5_obsclim_histsoc_default_burntarea-total_global_monthly_1901_2021.nc", callback=sort_time)[0]
-burnt_area=iris.cube.Cube([])
-
-
-bfiles=sorted( filter( os.path.isfile,
-                        glob.glob(pwdproc+"*burnt*") ) )
-ffiles=sorted( filter( os.path.isfile,
-                        glob.glob(pwdproc+"*_pft*monthly*2021*") ) )
-print(len(ffiles),len(bfiles))
-icount=0
-for ffile,bfile in zip(ffiles, bfiles):
-    if "burntarea-total" not in bfile:
-        print(ffile,bfile)
-        cube = iris.load(bfile, callback=sort_time)[0]*iris.load(ffile, callback=sort_time)[0]
-        #cube.data=cube.data/100.  
-        if icount == 0:
-            icount=1
-            burnt_area_pft = cube
-        else:
-            burnt_area_pft = cube + burnt_area_pft
-#iris.coord_categorisation.add_year(burnt_area_pft, 'time')
-#iris.coord_categorisation.add_year(burnt_area_total, 'time')
-burnt_area_pft=burnt_area_pft.extract(iris.Constraint(year=lambda cell: 2000 < cell < 2002 ))
-burnt_area_pft=burnt_area_pft.collapsed(["time"],iris.analysis.SUM)/100.0
-burnt_area_total=burnt_area_total.extract(iris.Constraint(year=lambda cell: 2000 < cell < 2002 ))
-burnt_area_total=burnt_area_total.collapsed(["time"],iris.analysis.SUM)
-print(burnt_area_pft.data)
-print(burnt_area_total.data)
-qplt.contourf(burnt_area_pft)
-iris.save(burnt_area_pft,"burnt_area_pft.nc")
-plt.show()
-qplt.contourf(burnt_area_total)
-iris.save(burnt_area_total,"burnt_area_total.nc")
-plt.show()
-try:
-    burnt_area_total.coord('latitude').guess_bounds()
-    burnt_area_total.coord('longitude').guess_bounds()
-except:
-    print("Already have weights")
-weights= iris.analysis.cartography.area_weights(burnt_area_total)
-print("from pft frac", burnt_area_pft.collapsed(['latitude','longitude'], iris.analysis.SUM, weights=weights).data/1e12)
-print("from global", burnt_area_total.collapsed(['latitude','longitude'], iris.analysis.SUM, weights=weights).data/1e12)
-
-
-## Some other basic functions
-
-#Load a file to see what's there
-cube = jules.load('/point/to/file/location/JULESannualfile.nc')
-print (cube)
-
-#load burnt area from the input file
-var_constraint = iris.Constraint(cube_func=lambda x: x.var_name == 'burnt_area_gb') 
-cube = jules.load_cube('/point/to/file/location/JULESannualfile.nc', var_constraint, missingdata=np.ma.masked)*86400*360 #multiply by seconds in a day and days in a JULES year
-cube.data[np.where(np.isnan(cube.data))] = 0.0
-print (cube)
-
-#plot a map of annual mean burnt area fraction 
-BAmap = cube.collapsed(['time'], iris.analysis.MEAN)
-qplt.pcolormesh(BAmap)
-plt.show()
-
-#load the landmask
-var_constraintx = iris.Constraint(cube_func=lambda x: x.var_name == 'field36')
-landfrac = iris.load_cube('/data/cr1/cburton/IMPORTANT/CRU-NCEPv7.landfrac.nc', var_constraintx)
-landfrac = landfrac.regrid(cube, iris.analysis.Linear()) 
-
-#Plot a timeseries of global annual total burnt area in Mkm2
-cube.data = cube.data * landfrac.data #multiply by land fraction to account for coastal gridboxes
-coords = ('longitude', 'latitude')
-for coord in coords:
-    if not cube.coord(coord).has_bounds():
-        cube.coord(coord).guess_bounds()
-weights = iris.analysis.cartography.area_weights(cube)
-cube = cube.collapsed(coords, iris.analysis.SUM, weights = weights) / 1E12 
-qplt.plot(cube)
-plt.show()
-
-
-#Other variables of interest:
-#veg_c_fire_emission_gb + burnt_carbon_rpm + burnt_carbon_dpm = total carbon emissions from vegetation (find the sum over lat and lon and divide by 1E12 to get GtC, no need to multiply by 86400 and 360 for these)
-#emission factors (check units):
-#fire_em_CO2_gb
-#fire_em_NOx_gb
-#fire_em_BC_gb
-#fire_em_CH4_gb
-#cv = vegetation cabon
-#precip
-#t1p5m_gb
-
-#Constrain to a region using natural earth continents or countries
-
-shpfilename = shpreader.natural_earth(resolution='110m', category='cultural', name='admin_0_countries')
-natural_earth_file = shape.load_shp(shpfilename)
-CountrySelect = shape.load_shp(shpfilename, Continent='South America')
-CountrySelect = Country.unary_union()
-CountrySelect.show()
-#or
-CountrySelect = shape.load_shp(shpfilename, Name='Brazil')
-CountryData = CountrySelect.mask_cube(cube)
-
-
-grid_weights = iris.analysis.cartography.area_weights(CountrySelect)
-#Collapse to find timeseries
-cube = CountrySelect.collapsed(coords, iris.analysis.SUM, weights = grid_weights1) / 1E12
-
-#Continent options = 'South America', 'Oceania', 'Europe', 'Afria', 'North America', 'Asia' 
 #Region mask notes:
 #https://regionmask.readthedocs.io/en/v0.9.0/
 #https://regionmask.readthedocs.io/en/v0.9.0/notebooks/mask_2D.html
 #https://regionmask.readthedocs.io/en/v0.9.0/defined_scientific.html
-
-
-#You can also cut out a section of the globe just using lat and long - this will give you a square rather than a specific country or biome:
-cube=cube.extract(iris.Constraint(latitude=lambda cell: (-60.0) < cell < (15.0), longitude=lambda cell: (275) < cell < (330))) # Whole South America
-cube=cube.extract(iris.Constraint(latitude=lambda cell: (-60.0) < cell < (10.0), longitude=lambda cell: (287) < cell < (326)))  #Brazil
-cube=cube.extract(iris.Constraint(latitude=lambda cell: (1.0) < cell < (5.0), longitude=lambda cell: (298) < cell < (302)))  #Manaus
-cube=cube.extract(iris.Constraint(latitude=lambda cell: (-17.0) < cell < (-13.0), longitude=lambda cell: (302) < cell < (306)))  #Primavera do Leste
-
-
-#total burnt area:
-try:
-    cube.coord('latitude').guess_bounds()
-    cube.coord('longitude').guess_bounds()
-except:
-    print("Already have weights")
-weights= iris.analysis.cartography.area_weights(cube)
-cube_out=cube.copy()
-cube_out.data=cube.data*weights
-print("total_burnt_area from total", cube_out.collapsed(['latitude','longitude'], iris.analysis.SUM).data/1e12, "million sq km2")
-
-
-# derived from pft fraction
-#print(cube_cf)
-#print(frac)
-cube_pft = cube_cf*frac
-try:
-    cube_pft.coord('latitude').guess_bounds()
-    cube_pft.coord('longitude').guess_bounds()
-except:
-    print("Already have weights")
-cube_pft = cube_pft.collapsed(['pft'], iris.analysis.SUM)
-#print(cube_pft)
-cube_out=cube_pft.copy()
-cube_out.data=cube_pft.data*weights
-print("from pft frac", cube_out.collapsed(['latitude','longitude'], iris.analysis.SUM).data/1e12)
 
 
