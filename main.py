9fb7b63a67947fb0e8fcbe86ea6cd18304056663
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2023-04-27 22:24:16 +0100

    indentong commentes

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import matplotlib.pyplot as plt
 
 
 def sub_year_months(data, months_of_year):
+     """Selects months of a year from data   
+    Arguments:
+        data -- either an iris cube or a 3-d numpy array where 3rd, or either format, 
+                the 3rd dimenion is months with first month being Janurary.
+        months_of_year -- numeric, month of the year you are interested in
+                from 0 (Jan) to 11 (Dec)
+    Returns:
+        data of just months we are interested in.
+    """
     months = []
     for month in months_of_year: months.append(np.arange(month, data.shape[2], 12))
     
@@ -16,18 +25,18 @@ def sub_year_months(data, months_of_year):
 
 def read_variable_from_netcdf(filename, dir = None, subset_function=None, *args, **kw):
     """Read data from a netCDF file 
-    Assumes that the variables in the netcdf file all have the name "variable"
-    Assunes that values < -9E9, you dont want. This could be different in some circumstances.
+        Assumes that the variables in the netcdf file all have the name "variable"
+        Assunes that values < -9E9, you dont want. This could be different in some circumstances
     Arguments:
-
-    filename -- a string with filename or two element python list containing the name of the file and the target variable name. If just the sting of "filename" assumes variable name is "variable"
-    dir -- directory file is in. Path can be in "filename" and None means no additional directory path needed.
+        filename -- a string with filename or two element python list containing the name of 
+            the file and the target variable name. If just the sting of "filename" 
+            assumes variable name is "variable"
+        dir -- directory file is in. Path can be in "filename" and None means no 
+            additional directory path needed.
     subset_function -- a function to be applied to each data set
 
     Returns:
-
-    Y - a numpy array of the target variable
-    X - an n-D numpy array of the feature variables 
+        Y - a numpy vector of the target variable
     """
 
     if isinstance(filename, str): filename = [filename, "variable"]
@@ -44,19 +53,19 @@ def read_variable_from_netcdf(filename, dir = None, subset_function=None, *args,
 def read_all_data_from_netcdf(y_filename, x_filename_list, add_1s_columne = False, 
                               y_threshold = None, *args, **kw):
     """Read data from netCDF files 
-    Assunes that values < -9E9, you dont want. This could be different in some circumstances.
+        Assunes that values < -9E9, you dont want. This could be different in some circumstances
     Arguments:
-
-    y_filename -- a two element python list containing the name of the file and the target variable name
-    x_filename_list -- a python list of filename containing the feature variables
-    y_threshold -- if converting y into boolean, the threshold we use to spit into 0's and 1's
-    add_1s_columne --useful for if using for regressions. Adds a variable of just 1's t rperesent y = SUM(a_i * x_i) + c
-    see read_variable_from_netcdf comments for *arg and **kw.
-
+        y_filename -- a two element python list containing the name of the file and the target 
+            variable name
+        x_filename_list -- a python list of filename containing the feature variables
+        y_threshold -- if converting y into boolean, the threshold we use to spit into 
+            0's and 1's
+        add_1s_columne --useful for if using for regressions. Adds a variable of 
+            just 1's t rperesent y = SUM(a_i * x_i) + c
+        see read_variable_from_netcdf comments for *arg and **kw.
     Returns:
-
-    Y - a numpy array of the target variable
-    X - an n-D numpy array of the feature variables 
+        Y - a numpy array of the target variable
+        X - an n-D numpy array of the feature variables 
     """
     Y = read_variable_from_netcdf(y_filename, *args, **kw)
     # Create a new categorical variable based on the threshold
@@ -91,17 +100,15 @@ def fit_linear_to_data(Y, X):
     """Use scikit learn to fit a linear equation.
 
     Arguments:
-
-    Y -- numpy array of target variables 
-    X -- numpy array of feature space variabels
+        Y -- numpy array of target variables 
+        X -- numpy array of feature space variabels
 
     Returns.
-
-    regr.coef_ -- regression coefficients
-    Y_pred -- model prediction of the Y values
+        regr.coef_ -- regression coefficients
+        Y_pred -- model prediction of the Y values
 
     **OR**
-    regr -- return the regression model
+        regr -- return the regression model
     """
 
 
@@ -121,17 +128,16 @@ def fit_logistic_to_data(Y, X):
     """Use scikit learn to fit a linear equation.
 
     Arguments:
-
-    Y -- numpy array of target variables 
-    X -- numpy array of feature space variabels
+        Y -- numpy array of target variables 
+        X -- numpy array of feature space variabels
 
     Returns.
 
-    logr.coef_ -- regression coefficients
-    Y_pred -- model prediction of the Y values
+        logr.coef_ -- regression coefficients
+        Y_pred -- model prediction of the Y values
 
     **OR**
-    logr -- return the regression model
+        logr -- return the regression model
     """
     
    
@@ -154,15 +160,15 @@ if __name__=="__main__":
                   "lightn.nc", "rhumid.nc", "cveg.nc", "pas.nc", "soilM.nc", 
                    "totalVeg.nc", "popDens.nc", "trees.nc"]
 
-    Y, X=read_all_data_from_netcdf(y_filen, x_filen_list, add_1s_columne = True, dir = dir, 
-                                   y_threshold = 0.01, subset_function = sub_year_months, 
-                                   months_of_year = [6, 7, 8])
+    Y, X = read_all_data_from_netcdf(y_filen, x_filen_list, add_1s_columne = True, dir = dir, 
+                                     y_threshold = 0.01, subset_function = sub_year_months, 
+                                     months_of_year = [6, 7, 8])
     
     #reg = fit_linear_to_data(Y, X)
     #plt.plot(Y, reg.predict(X), '.')
     
     logr = fit_logistic_to_data(Y, X)
-    plt.plot(logr.predict_proba(X)[:,1],Y, '.')
+    plt.plot(logr.predict_proba(X)[:,1], Y, '.')
     
     plt.show()
     print(Y)
