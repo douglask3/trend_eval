0534ed63527ffa6fc0dd72b1ec02eef161c170aa
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2023-04-27 13:47:52 +0100

    sorting out Readme for constrain into and constrains code comments

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Coding-python
+
+
+## Notes on constraining area
+
+
+### Political areas
+When opening data, setting ''Country'' or ''Continent'' will constrain the extent to that country or continent, and mask areas outside of it. Uses Natural Earth. If you define a country, it one look at the continent. Use None is you don't want any. Continent options are:
+* 'South America'
+* 'Oceania'
+* 'Europe'
+* 'Afria'
+* 'North America'
+* 'Asia'
+
+### Ecoregions
+''ecoregions'' is a numeric list (i.e [3, 7, 8]) where numbers pick Olson bomes and mask out everywhere else. If you  pick more than one, it returns a map of all of them.
+* **None** Return all areas.
+* **1** Tropical and subtropical moist broadleaf forests
+* **2** Tropical and subtropical dry broadleaf forests
+* **3** Tropical and suptropical coniferous forests
+* **4** Temperate broadleaf and mixed forests
+* **5** Temperate Coniferous Forest
+* **6** Boreal forests / Taiga
+* **7** Tropical and subtropical grasslands, savannas and shrublands
+* **8** Temperate grasslands, savannas and shrublands
+* **9** Flooded grasslands and savannas
+* **10** Montane grasslands and shrublands
+* **11** Tundra
+* **12** Mediterranean Forests, woodlands and scrubs
+* **13** Deserts and xeric shrublands
+* **14** Mangroves
+
--- libs/constrain_cubes_standard.py
+++ libs/constrain_cubes_standard.py
@@ -10,6 +10,32 @@ from ascend import shape
 import numpy as np
 
 def constrain_olson(cube, ecoregions):
+     """constrains a cube to Olson ecoregion
+    Assumes that the cube is iris and on a 0.5 defree grid
+
+    Arguments:
+
+    cube -- an iris cube at 0.5 degrees
+    ecoregions -- numeric list (i.e [3, 7, 8]) where numbers pick Olson biomes.
+        You can pick more than one:
+            1 Tropical and subtropical moist broadleaf forests
+            2 Tropical and subtropical dry broadleaf forests
+            3 Tropical and suptropical coniferous forests
+            4 Temperate broadleaf and mixed forests
+            5 Temperate Coniferous Forest
+            6 Boreal forests / Taiga
+            7 Tropical and subtropical grasslands, savannas and shrublands
+            8 Temperate grasslands, savannas and shrublands
+            9 Flooded grasslands and savannas
+            10 Montane grasslands and shrublands
+            11 Tundra
+            12 Mediterranean Forests, woodlands and scrubs
+            13 Deserts and xeric shrublands
+            14 Mangroves
+
+    Returns:
+    Input cube with areas outside of selected Olson biomes masked out.
+    """
     biomes = iris.load_cube('data/wwf_terr_ecos_0p5.nc')
     mask = biomes.copy()
     mask.data[:] = 0.0
@@ -22,9 +48,32 @@ def constrain_olson(cube, ecoregions):
         layer[mask] = np.nan
     return cube
 
-def constrain_natural_earth(cube, Continent, Country):
-    shpfilename = shpreader.natural_earth(resolution='110m', 
-                                          category='cultural', name='admin_0_countries')
+def constrain_natural_earth(cube, Country, Continent = None, shpfilename = None):
+    
+     """constrains a cube to Natural Earth Country or continent.
+    Assumes that the cube is iris
+    If Country is defined, it wont select Continent.
+
+    Arguments:
+
+    cube -- an iris cube
+    Continent -- name of continent. Options are:
+        'South America'
+        'Oceania'
+        'Europe'
+        'Afria'
+        'North America'
+        'Asia' 
+    shpfilename -- path and filename of natural earth shapefile.
+                   If set to None, it will look for temp. file version and 
+                   download if it does not exist.
+    Returns:
+    Input cube constrained to the extent to that country or continent, 
+    and mask areas outside of it. Uses Natural Earth
+    """
+    if shpfilename is None:
+        shpfilename = shpreader.natural_earth(resolution='110m', 
+                                              category='cultural', name='admin_0_countries')
     natural_earth_file = shape.load_shp(shpfilename)
     if Country is not None:
         NAMES = [i.attributes.get('NAME') for i in natural_earth_file]
@@ -38,11 +87,23 @@ def constrain_natural_earth(cube, Continent, Country):
     cube = CountrySelect[0].mask_cube(cube)
     return cube
 
-def constrain_region(cube, ecoregions, Continent, Country):
+def constrain_region(cube, ecoregions = None, Country = None, Continent = None, *args, **kw):
+     """ checks if any spatial constrains are set and contrains according to comments in functions above
+
+    Arguments:
+
+    cube -- an iris cube
+    ecoregions -- see constrain_olson help
+    Country, Continent -- see constrain_natural_earth help
+      
+    Returns:
+    Input cube constrained to the extent to defined country or continent, 
+    and mask areas outside of it and defined ecoregions.
+    """
     if ecoregions is not None:
         cube = constrain_olson(cube, ecoregions)
 
     if Continent is not None or Country is not None:
-        cube = constrain_natural_earth(cube, Continent, Country)
+        cube = constrain_natural_earth(cube, Country, Continent, *args, **kw)
 
     return(cube)
