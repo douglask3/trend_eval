fd13f01d919ceb696a102d23c2b91860126c584f
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2023-05-28 08:19:19 +0100

    adding comments

--- libs/plot_AR6_hexagons.py
+++ libs/plot_AR6_hexagons.py
@@ -8,6 +8,26 @@ import matplotlib.pyplot as plt
 def plot_AR6_hexagons(result, resultID = 1, vmin = 0.0, vcenter = 50.0, vmax = 100.0,
                       cmap = 'RdYlBu', add_colorbar = True, colorbar_label = '',
                       annotate_hexagons = True):  
+    """plots AR6 region based results in a hexagon map
+        Assumes the AR6 region id is in the first element of "result" input
+        Colorbar is just linear at the mo. I'll change this at some point.
+    Arguments:
+        result -- a 2-d list of results you want to plot. Each element in list has at least 2 
+                    entries. The first is the region ID as a string and the 2nd is the 
+                    numeric result you want to plotin that region
+        resultID -- integer. The entry poistion each each result element that you want to plot.                     Default set to one assuming most the the time, result will be region ID 
+                    and result only.
+        vmin --- the minimum value in the colourmap
+        vcenter -- the minimum value in the colourmap
+        vmax -- the maximum value in teh colourmap
+        cmap -- colourmap
+        add_colorbar -- Boolean. If True, add colourbar legend to the bottom of the map
+        colorbar_label -- String. If 'add_colorbar' is True, then this is the label added to 
+                    the colorbar
+        annotate_hexagons -- Boolean. If True, addes region IDs to hexagons.
+    Returns:
+        Hexogan map of results
+    """
     hexagons = gpd.read_file('data/WeightedZones.gpkg').copy()
     hexagons['Model_results'] = np.nan
     for res in result:
