814e204a7e829fd80d7630dc6f699dbf7d92993d
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2023-05-31 16:22:26 +0100

    merging n some changes from commit #65764a11c235639eadc3f4d1da587524f19d7e85 in trends_eval branch which allows for ignoring mask values when opening mulple files.

--- libs/read_variable_from_netcdf.py
+++ libs/read_variable_from_netcdf.py
@@ -60,9 +60,9 @@ def read_variable_from_netcdf(filename, dir = '', subset_function = None,
     return dataset
 
 def read_all_data_from_netcdf(y_filename, x_filename_list, add_1s_columne = False, 
-                              y_threshold = None, *args, **kw):
+                              y_threshold = None, check_mask = True, *args, **kw):
     """Read data from netCDF files 
-        Assunes that values < -9E9, you dont want. This could be different in some circumstances
+        
     Arguments:
         y_filename -- a two element python list containing the name of the file and the target 
             variable name
@@ -71,12 +71,16 @@ def read_all_data_from_netcdf(y_filename, x_filename_list, add_1s_columne = Fals
             0's and 1's
         add_1s_columne --useful for if using for regressions. Adds a variable of 
             just 1's t rperesent y = SUM(a_i * x_i) + c
+        check_mask -- Boolean. If True, simple checks if there are any large negtaive numbers 
+            and makes them out. Assunes that values < -9E9, you dont want. This could be different 
+            in some circumstances
         see read_variable_from_netcdf comments for *arg and **kw.
     Returns:
         Y - a numpy array of the target variable
         X - an n-D numpy array of the feature variables 
     """
     Y = read_variable_from_netcdf(y_filename, make_flat = True, *args, **kw)
+   
     # Create a new categorical variable based on the threshold
     if y_threshold is not None:
         Y = np.where(Y >= y_threshold, 0, 1)
@@ -86,18 +90,19 @@ def read_all_data_from_netcdf(y_filename, x_filename_list, add_1s_columne = Fals
     
     n=len(Y)
     m=len(x_filename_list)
-    X=np.zeros([n,m])
-
+    
+    X = np.zeros([n,m])
+    
     for i, filename in enumerate(x_filename_list):
         X[:, i]=read_variable_from_netcdf(filename, make_flat = True, *args, **kw)
-    
-    cells_we_want = np.array([np.all(rw > -9e9) for rw in np.column_stack((X, Y))])
-    Y = Y[cells_we_want]
-    X = X[cells_we_want, :]
 
     if add_1s_columne: 
         X = np.column_stack((X, np.ones(len(X)))) # add a column of ones to X 
+    
+    if check_mask:
+        cells_we_want = np.array([np.all(rw > -9e9) for rw in np.column_stack((X, Y))])
+        Y = Y[cells_we_want]
+        X = X[cells_we_want, :]
 
-    return Y, X, cells_we_want
-
-
+        return Y, X, cells_we_want
+    return Y, X
